3bbafc513b9467c24ed8e66bcf3825ccbf705276
Aluno <Aluno@10.85.198.36>
2024-06-06 14:13:26 -0300

    shopping

---
+++ src/Variables.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+
+    
+
+ 
+    public static void main(String[] args) {
+        String custName = "Alex";
+        String itemDesc = "Shirts";
+        String message = custName+" wants to purchase a "+itemDesc;
+        
+        // Declare and initialize numeric fields: price, tax, quantity.   
+        
+        
+        
+        // Declare and assign a calculated totalPrice
+        
+        
+        // Modify message to include quantity 
+        
+        System.out.println(message);
+        
+        // Print another message with the total cost
+        
+    }    
+ 
+
+
+  
+   
+  
+
